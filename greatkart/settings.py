be7f6403a0a4fb3a9d862116b35e0bbb7d96a12d
Herbertalbuquerque <herbertrj87@icloud.com>
2024-02-02 20:11:36 -0300

    django athentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,17 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP CONFIGURATION
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'herbertrj26@gmail.com'
+EMAIL_HOST_PASSWORD = 'wssz fbmq ydth qygs'
+EMAIL_USE_TLS = True
+ 
