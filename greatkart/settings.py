e2e08b76200970c2d9e9e20eaacb1a6759dcacc8
Herbertalbuquerque <herbertrj87@icloud.com>
2024-02-09 19:37:25 -0300

    atualicao

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # my apps
     'category',
     'accounts',
     'store',
     'carts',
+    'orders',
     
 ]
 
